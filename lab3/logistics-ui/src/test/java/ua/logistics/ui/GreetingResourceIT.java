75f70d72c660e76348e24f670465614312d68ae4
Arsen <arsen.khomiak@gmail.com>
2025-12-13 16:25:51 +0200

    Init Lab 4: Create Logistics UI microservice

---
+++ lab3/logistics-ui/src/test/java/ua/logistics/ui/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ua.logistics.ui;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
